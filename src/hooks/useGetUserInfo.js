8e37616910f9be1a2ba8c1c1647eb6b127d27224
Mahapara <mahanizamani12345678@gmail.com>
2024-03-09 19:49:48 +0500

    updated usegetuserinfo

--- src/hooks/useGetUserInfo.js
+++ src/hooks/useGetUserInfo.js
@@ -1,7 +1,15 @@
+// export const useGetuserInfo = () => {
+//   const { name, profilePhoto, userID, isAuth } = JSON.parse(
+//     localStorage.getItem("auth") || {}
+//   );
+
+//   return { name, profilePhoto, userID, isAuth };
+// };
 export const useGetuserInfo = () => {
-  const { name, profilePhoto, userID, isAuth } = JSON.parse(
-    localStorage.getItem("auth") || {}
-  );
+  const authData = localStorage.getItem("auth");
+  const { name, profilePhoto, userID, isAuth } = authData
+    ? JSON.parse(authData)
+    : {};
 
   return { name, profilePhoto, userID, isAuth };
 };
